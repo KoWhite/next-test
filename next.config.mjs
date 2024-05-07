1fb77f03d93b66b98f85a551b641cb00b43fbc9c
MichLiu <liuqiquan@4399inc.com>
2024-05-07 11:27:17 +0800

    feat: first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  // 严格模式
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  sassOptions: {
+    additionalData: '@import "@/styles/index.scss";',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', //图片资源的协议
+        hostname: 'www.test.com', //图片资源的域名
+      },
+    ],
+  },
+};
 
 export default nextConfig;
